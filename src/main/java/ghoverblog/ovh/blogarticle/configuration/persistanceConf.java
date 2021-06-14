a95a520a46bce05ffb968213a24eb04ece02f799
Maxime <59308346+MGNetworking@users.noreply.github.com>
2021-06-14 17:47:57 +0200

    exposition de l'identifiant de l'article

---
+++ src/main/java/ghoverblog/ovh/blogarticle/configuration/persistanceConf.java
@@ -0,0 +1,23 @@
+package ghoverblog.ovh.blogarticle.configuration;
+
+import ghoverblog.ovh.blogarticle.entities.Article;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+
+@Configuration
+public class persistanceConf implements RepositoryRestConfigurer {
+
+    /**
+     * Cette configuration permet d'exposer l'id de l'objet article.
+     *
+     * @param config
+     * @param cors
+     */
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Article.class);
+    }
+}
