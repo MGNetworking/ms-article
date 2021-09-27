d2fe3ed92b980e49267afb23e8320a9c1226bd33
Maxime <59308346+MGNetworking@users.noreply.github.com>
2021-09-27 11:14:57 +0200

    ajout du end point /article-config/config-service

--- src/main/java/ArticleWebService/web/ControllerArticleService.java
+++ src/main/java/ArticleWebService/web/ControllerArticleService.java
@@ -1,7 +1,6 @@
 package ArticleWebService.web;
 
 import ArticleWebService.component.ConfigurationArticle;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
