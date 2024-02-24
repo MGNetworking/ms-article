725cbcde9d0dddb4c99f73af2b94762339bece12
Maxime <ghalem.maxime@gmail.com>
2024-02-24 22:18:30 +0100

    suppression des logs vers les sortis externe

--- script/healthcheck.sh
+++ script/healthcheck.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
-LOG_FILE="/logs/healthcheck.log"
-
-echo  "script healthcheck en cours ... " >> "$LOG_FILE"
-
+echo  "script healthcheck en cours ... "
 
 status=$(curl -s -m 30 http://ms-article:9010/actuator/health | jq -r '.status' )
-echo " Résultat de la requête curl : $status" >> "$LOG_FILE"
+echo " Résultat de la requête curl : $status"
 
 if [ "$status" = "UP" ]; then
-  echo  "success du script de santé : $status" >> "$LOG_FILE"
+  echo  "success du script de santé : $status"
   exit 0  # Succès
  elif [ "$status" = "DOWN" ]; then
-  echo  "Échec du script de santé : $status" >> "$LOG_FILE"
+  echo  "Échec du script de santé : $status"
   exit 1  # Échec 1
   else
-    echo  "en cours de démarrage : $status" >> "$LOG_FILE"
+    echo  "en cours de démarrage : $status"
     exit 0  # Succès
 fi
