b48e43364f859ecc0ef993817c7b46d506a074f8
Maxime <ghalem.maxime@gmail.com>
2024-03-19 22:20:06 +0100

    correction en cours ...

--- script/wait_for_config.sh
+++ script/wait_for_config.sh
@@ -2,16 +2,19 @@
 
 echo  "Lancement du script wait_for_config en cours ... "
 
+# Dans le cas d'un déployement sur le nas
 if [ -z "$PROFILE_ACTIF_SPRING" ]; then
 
   echo "La variable PROFILE_ACTIF_SPRING => $PROFILE_ACTIF_SPRING <= est absente "
   PROFILE_ACTIF_SPRING=nas
   echo "La variable PROFILE_ACTIF_SPRING est maintenant initialiser => $PROFILE_ACTIF_SPRING | "
+  IP=172.17.0.1
+  echo "valeur de l'ip de connection a la base de données :  $IP "
 fi
 
+echo "Initialisation de l'adresse Ip du service configuration sur le réseau Overlay"
 SERVICE_CONFIG_DOCKER=http://ms-configuration:8089
 echo "La variable SERVICE_CONFIG_DOCKER est maintenant initialiser => $SERVICE_CONFIG_DOCKER |"
-echo "valeur de l'ip de connection a la base de données :  $IP "
 
   while true; do
     response=$(curl -s $SERVICE_CONFIG_DOCKER/msarticle/$PROFILE_ACTIF_SPRING)
