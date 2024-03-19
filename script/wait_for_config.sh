8b51303ab76310c9291d5f78ae14a4030fc0a69f
Maxime <ghalem.maxime@gmail.com>
2024-03-20 00:30:24 +0100

    update

--- script/wait_for_config.sh
+++ script/wait_for_config.sh
@@ -28,7 +28,6 @@ echo "La variable SERVICE_CONFIG_DOCKER est maintenant initialiser => $SERVICE_C
     if [ -n "$response" ]; then
       echo "Le service est en cours d'exécution."
       echo "Lancement du service article ..."
-      #java -jar app.jar --spring.profiles.active=$PROFILE_ACTIF_SPRING -DIP=$IP
       java -jar app.jar --spring.profiles.active=$PROFILE_ACTIF_SPRING --IP=$IP
 
       break  # Sortir de la boucle si le service est opérationnel
