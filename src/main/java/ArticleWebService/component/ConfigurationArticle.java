d833784b07281227f2e50baea9f81b239f508711
Maxime <ghalem.maxime@gmail.com>
2024-12-06 08:16:42 +0100

    clear comment

--- src/main/java/ArticleWebService/component/ConfigurationArticle.java
+++ src/main/java/ArticleWebService/component/ConfigurationArticle.java
@@ -33,61 +33,11 @@ public class ConfigurationArticle {
                 .distinct()
                 .forEach(propName -> {
 
-                            log.info(propName + " : " + environment.getProperty(propName));
-
-                            // info.profile == prod , ne pas rendre visible en prod du password data Base
-  /*                          if (!environment.getProperty(propName).equals("prod")) {
-                                // propName = datasource.password  , spring.datasource.password
-                                param.put(propName, environment.getProperty(propName));
-                            }*/
+                            log.info("{} : {}", propName, environment.getProperty(propName));
                             param.put(propName, environment.getProperty(propName));
 
                         }
                 );
-
-/*
-        //
-        param.put("name", environment.getProperty("spring.application.name"));
-        param.put("profile", environment.getProperty("info.profile"));
-        param.put("server port", environment.getProperty("server.port"));
-        param.put("storage article location", environment.getProperty("storage-article.location"));
-
-        // logging
-        param.put("path logging file", environment.getProperty("logging.file.path"));
-        param.put("Level logging Controller Spring", environment.getProperty("logging.level.org.springframework.controller"));
-        param.put("Level logging hibernate", environment.getProperty("logging.level.org.hibernate"));
-
-        // actuator and devtool
-        param.put("actuator management include", environment.getProperty("management.endpoints.web.exposure.include"));
-        param.put("devtools restart enabled", environment.getProperty("spring.devtools.restart.enabled"));
-
-        // data jpa
-        param.put("datasource url", environment.getProperty("spring.datasource.url"));
-        param.put("datasource username", environment.getProperty("spring.datasource.username"));
-
-        if (!environment.getProperty("info.profile").equals("prod")) {
-            param.put("datasource password", environment.getProperty("spring.datasource.password"));
-        }
-        param.put("datasource driver-class-name", environment.getProperty("spring.datasource.driver-class-name"));
-
-        param.put("jpa show-sql", environment.getProperty("spring.jpa.show-sql"));
-        param.put("jpa properties hibernate format_sql", environment.getProperty("spring.jpa.properties.hibernate.format_sql"));
-
-        // keyclaok
-        param.put("keycloak realm", environment.getProperty("keycloak.realm"));
-        param.put("keycloak cors", environment.getProperty("keycloak.cors"));
-        param.put("keycloak resource", environment.getProperty("keycloak.resource"));
-        param.put("keycloak auth-server-url", environment.getProperty("keycloak.auth-server-url"));
-        param.put("keycloak public-client", environment.getProperty("keycloak.public-client"));
-
-        // eureka
-        param.put("eureka instance-id", environment.getProperty("eureka.instance.instance-id"));
-        param.put("eureka client service-url defaultZone", environment.getProperty("eureka.client.service-url.defaultZone"));
-        param.put("spring cloud discovery enabled", environment.getProperty("spring.cloud.discovery.enabled"));
-
-        // serveur
-        param.put("server forward-headers-strategy", environment.getProperty("server.forward-headers-strategy"));
-        param.put("storage-article location", environment.getProperty("storage-article.location"));*/
 
         return param;
     }
