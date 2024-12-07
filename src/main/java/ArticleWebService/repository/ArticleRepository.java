253ec628756702e78acfbd2fd87b1cd3b0f1d9fa
Maxime <ghalem.maxime@gmail.com>
2024-12-07 15:20:33 +0100

    clear: delete old comment !

--- src/main/java/ArticleWebService/repository/ArticleRepository.java
+++ src/main/java/ArticleWebService/repository/ArticleRepository.java
@@ -1,36 +1,21 @@
 package ArticleWebService.repository;
 
 import ArticleWebService.entities.Article;
-import ArticleWebService.entities.Section;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
-
 
     @Query(value = "SELECT art FROM Article art WHERE art.section.idSection = :sect ORDER BY art.idArticle")
     Page<Article> findAllArticlesBySection(Pageable pageable, @Param("sect") Integer section);
 
     @Query(value = "SELECT art FROM Article art ORDER BY art.idArticle")
     Page<Article> findAllArticlePageOrderBy(Pageable pageable);
-
-    List<Article> findBySectionIdSection(Integer section);
-
-/*
-    @Query(value = "SELECT art FROM article art " +
-            "LEFT JOIN FETCH art.section sect WHERE sect.id_section =:section ORDER BY art.id_article",
-            nativeQuery = true)
-    Page<Article> findAllArticlesBySection(Pageable pageable, @Param("section") int section);
-*/
-
 
 }
--- src/main/java/ArticleWebService/repository/DomainRepository.java
+++ src/main/java/ArticleWebService/repository/DomainRepository.java
@@ -2,12 +2,7 @@ package ArticleWebService.repository;
 
 import ArticleWebService.entities.Domain;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface DomainRepository extends JpaRepository<Domain, Integer> {
