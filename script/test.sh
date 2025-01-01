0a8c2b58414102ce90b6505f095c80c46d01e388
maxime-wsl <ghalem.maxime>
2025-01-01 19:01:20 +0100

    update script

--- script/test.sh
+++ script/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Les tests unitaire classique avec le profile spring  dev
+echo Les tests unitaire classique avec le profile spring  dev
 mvn clean test -Dspring.profiles.active=dev
 
-# Les test unitaire end t end avec le profile spring dev
+echo Les test unitaire end t end avec le profile spring dev
 mvn clean verify -P e2e -Dspring.profiles.active=dev
 
-# Les test unitaire d'integration avec le profile spring dev
+echo Les test unitaire d\'integration avec le profile spring dev
 mvn clean verify -P integration -Dspring.profiles.active=dev
 
