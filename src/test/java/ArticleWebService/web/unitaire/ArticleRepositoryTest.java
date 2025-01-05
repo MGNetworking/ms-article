73f456e0a40dc61ae243a56abe920c67c6deb426
maxime-wsl <ghalem.maxime>
2025-01-05 11:16:44 +0100

    add: Test ArticleRepository

---
+++ src/test/java/ArticleWebService/web/unitaire/ArticleRepositoryTest.java
@@ -0,0 +1,16 @@
+package ArticleWebService.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticleRepositoryTest {
+
+    @Test
+    void findAllArticlesBySection() {
+    }
+
+    @Test
+    void findAllArticlePageOrderBy() {
+    }
+}
