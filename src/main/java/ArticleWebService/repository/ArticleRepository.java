57a42e7fe6b9ac678fb7bcffb572d5adca48e9df
Maxime <ghalem.maxime@gmail.com>
2025-01-22 14:32:36 +0100

    add: JPQL updateArticleMeta and update: updateArticleFields

--- src/main/java/ArticleWebService/repository/ArticleRepository.java
+++ src/main/java/ArticleWebService/repository/ArticleRepository.java
@@ -1,12 +1,17 @@
 package ArticleWebService.repository;
 
+import ArticleWebService.dto.ArticleDtoUpdate;
 import ArticleWebService.entities.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 
 @Repository
@@ -17,5 +22,32 @@ public interface ArticleRepository extends JpaRepository<Article, Integer> {
 
     @Query(value = "SELECT art FROM Article art ORDER BY art.idArticle")
     Page<Article> findAllArticlePageOrderBy(Pageable pageable);
+
+    /**
+     * Permet de mettre à jour les champs d'article.
+     *
+     * @param dto l'objet de mise à jour.
+     * @return Un entier pour le nombre de lignes affecté.
+     */
+    @Modifying
+    @Transactional
+    @Query("UPDATE Article a SET " +
+            "a.titre = COALESCE(:#{#dto.titre}, a.titre), " +
+            "a.article = COALESCE(:#{#dto.article}, a.article), " +
+            "a.imgUrl = COALESCE(:#{#dto.imgUrl}, a.imgUrl), " +
+            "a.imgDescription = COALESCE(:#{#dto.imgDescription}, a.imgDescription), " +
+            "a.description = COALESCE(:#{#dto.description}, a.description), " +
+            "a.dateMaj = CURRENT_TIMESTAMP " +
+            "WHERE a.section.idSection = :#{#dto.section.idSection} AND a.idArticle = :#{#dto.idArticle}")
+    int updateArticleFields(@Param("dto") ArticleDtoUpdate dto);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Article a SET " +
+            "a.vue = COALESCE(a.vue + :#{#dto.vue}, a.vue), " +
+            "a.isVisibale = COALESCE(:#{#dto.isVisibale}, a.isVisibale) " +
+            "WHERE a.section.idSection = :#{#dto.section.idSection} AND a.idArticle = :#{#dto.idArticle}")
+    int updateArticleMeta(@Param("dto") ArticleDtoUpdate dto);
+
 
 }
